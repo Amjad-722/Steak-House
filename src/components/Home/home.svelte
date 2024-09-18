7049bab362b39cce330ced812016a9e4c2e8d69d
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-18 19:48:25 +0500

    updated home

--- src/components/Home/home.svelte
+++ src/components/Home/home.svelte
@@ -2,16 +2,16 @@
 
 </script>
 
-<div class="flex flex-col justify-center items-center h-screen text-center pt-50"
+<div class="flex flex-col container justify-start  items-center h-screen text-center pt-60"
 >
-  <p class="text-2xl text-fontColor font-bold mb-6 max-w-lg pt-6">
-    The Boutique Store For Plants!
+  <p class="text-7xl text-fontColor flex justify-start font-bold mb-6 max-w-lg pt-6">
+    A Premium
   </p>
 
   <h1 class="text-8xl text-fontColor font-bold mb-4 pt-6">
-    Everything Is Better
+    And Authentic
     <br />
   </h1>
   <h1 class="text-8xl text-fontColor
-   font-bold mb-4 pt-6">With Plants</h1>
+   font-bold mb-4 pt-6">Steak House</h1>
 </div>
