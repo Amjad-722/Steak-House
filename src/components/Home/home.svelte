f116ddfea7f932709e304b26cdabd84d3fe4e0ff
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-19 20:20:32 +0500

    updated

--- src/components/Home/home.svelte
+++ src/components/Home/home.svelte
@@ -1,9 +1,13 @@
 <script>
+  import Button from "../Button/button.svelte";
+
 let heading="A Premium and Authentic Steak House" 
 </script>
 
-<div class="flex flex-col container justify-start  items-center h-screen text-center pt-60"
+<div class="flex flex-col container justify-start  mx-auto pl-40 items-center h-screen text-center pt-60"
 >
-<h1 class="text-5xl font-bold text-fontColor"> {heading}</h1>  
-  
+<h1 class="text-8xl font-extrabold text-fontColor"> {heading}</h1>  
+
+  <Button size="lg"   className="mt-6">
+    Make Reservation  </Button>
 </div>
