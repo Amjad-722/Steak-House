f3989fb9858c2c2aa787f97c51aa7a41a0f5c3cd
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-19 21:49:34 +0500

    updated

--- src/components/Home/home.svelte
+++ src/components/Home/home.svelte
@@ -1,21 +1,8 @@
+
 <script>
+  import Button from "../Button/button.svelte";
 
-<<<<<<< homee
-</script>
 
-<div class="flex flex-col container justify-start  items-center h-screen text-center pt-60"
->
-  <p class="text-7xl text-fontColor flex justify-start font-bold mb-6 max-w-lg pt-6">
-    A Premium
-  </p>
-
-  <h1 class="text-8xl text-fontColor font-bold mb-4 pt-6">
-    And Authentic
-    <br />
-  </h1>
-  <h1 class="text-8xl text-fontColor
-   font-bold mb-4 pt-6">Steak House</h1>
-=======
   let headingLine1 = "A Premium";
   let headingLine2 = "and Authentic";
   let headingLine3 = "Steak House";
@@ -31,5 +18,5 @@
   <Button size="lg" className="mt-10">
     Make Reservation
   </Button>
->>>>>>> local
+
 </div>
