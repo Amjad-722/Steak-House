635c5f50f6f41722590f02ecf40913b4a1d20238
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-19 21:22:53 +0500

    updated

--- src/components/Home/home.svelte
+++ src/components/Home/home.svelte
@@ -1,5 +1,6 @@
 <script>
 
+<<<<<<< homee
 </script>
 
 <div class="flex flex-col container justify-start  items-center h-screen text-center pt-60"
@@ -14,4 +15,21 @@
   </h1>
   <h1 class="text-8xl text-fontColor
    font-bold mb-4 pt-6">Steak House</h1>
+=======
+  let headingLine1 = "A Premium";
+  let headingLine2 = "and Authentic";
+  let headingLine3 = "Steak House";
+</script>
+
+<div class="flex flex-col container justify-start mx-auto pl-40 items-start h-screen text-left pt-60">
+  <h1 class="text-8xl font-extrabold leading-medium text-fontColor">
+    <span>{headingLine1}</span><br>
+    <span>{headingLine2}</span><br>
+    <span>{headingLine3}</span>
+  </h1>
+
+  <Button size="lg" className="mt-10">
+    Make Reservation
+  </Button>
+>>>>>>> local
 </div>
