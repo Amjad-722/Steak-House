e67173333a2d6abe4fac608c0d81d05c2190ac5e
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-19 13:33:41 +0500

    updated

---
+++ src/components/Button/button.svelte
@@ -0,0 +1,17 @@
+<script>
+    
+   
+    export let disabled = false; 
+    export let className = 'click me';   
+  </script>
+  
+  
+  <button
+    
+    class={`px-12 py-5 hover:bg-white  border-2 hover:border-gray-500 text-white font-semibold  disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
+    disabled={disabled}
+    on:click  
+  >
+    <slot></slot> 
+  </button>
+  
