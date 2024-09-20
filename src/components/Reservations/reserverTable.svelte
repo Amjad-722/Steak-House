436aaeef04b0a3fe059c5fe3f0163d92b8aaad2a
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-20 18:53:27 +0500

    uppdated

---
+++ src/components/Reservations/reserverTable.svelte
@@ -0,0 +1,27 @@
+<script>
+
+let headingLine1 = "Welcome ";
+let headingLine2 = "to Steak by ";
+let headingLine3 = " the Bay";
+</script>
+
+
+<main> 
+<div class="container flex flex-row gap-20 mx-auto pt-20 ">
+    <div>
+        <div class="flex flex-col justify-center items-center">
+            <h1 class="text-6xl font-extrabold leading-snug sm:leading-snug md:leading-medium lg:leading-medium text-fontColor">{headingLine1}
+                <span><br>{headingLine2}</span>
+                <span> <br>{headingLine3}</span>
+            </h1>
+            <img class="mt-10 w-96 border-4 placeholder:border-fontColor" src="https://websitedemos.net/steak-house-02/wp-content/uploads/sites/841/2021/05/about.jpg" alt="">
+
+        </div>
+    </div>
+    <div>
+        <h1 class="text-4xl font-extrabold leading-snug sm:leading-snug md:leading-medium lg:leading-medium text-fontColor">{headingLine1}</h1>
+    </div>
+</div>
+
+
+</main>
