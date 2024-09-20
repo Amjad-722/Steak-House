a32b6e4659dcdc01778e2613f4e813d5c0da4531
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-20 18:35:29 +0500

    uppdated

--- src/components/Home/home.svelte
+++ src/components/Home/home.svelte
@@ -5,14 +5,14 @@
   let headingLine3 = "Steak House";
 </script>
 
-<div class="flex flex-col container mx-auto pl-5 sm:pl-10 md:pl-20 lg:pl-40 items-start h-screen text-left pt-20 sm:pt-40 md:pt-60 justify-center">
+<div class="flex flex-col container mx-auto pl-5 sm:pl-10 md:pl-20 lg:pl-40 items-start h-screen text-left pt-16 sm:pt-40 md:pt-60 justify-center">
   <h1 class="text-4xl sm:text-5xl md:text-7xl lg:text-8xl font-extrabold leading-snug sm:leading-snug md:leading-medium lg:leading-medium text-fontColor">
     <span>{headingLine1}</span><br>
     <span>{headingLine2}</span><br>
     <span>{headingLine3}</span>
   </h1>
 
-  <Button size="lg" class="mt-6 sm:mt-8 md:mt-10">
+  <Button size="lg" class="mt-8 sm:mt-8 md:mt-10">
     Make Reservation
   </Button>
 </div>
