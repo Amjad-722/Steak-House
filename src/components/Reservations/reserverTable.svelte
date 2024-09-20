927d2f8f90c4f7a7b9e3043bdc815af46eff78a7
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-20 20:23:44 +0500

    updated

--- src/components/Reservations/reserverTable.svelte
+++ src/components/Reservations/reserverTable.svelte
@@ -19,14 +19,14 @@
         <img class="mt-16 w-96 border-4 border-fontColor" src="https://websitedemos.net/steak-house-02/wp-content/uploads/sites/841/2021/05/about.jpg" alt="Steak by the Bay">
       </div>
       <div class="flex flex-col justify-center items-center">
-        <h1 class="text-6xl font-extrabold leading-snug sm:leading-snug md:leading-medium lg:leading-medium text-fontColor">
+        <h1 class="text-6xl font-bold leading-snug sm:leading-snug md:leading-medium lg:leading-medium text-fontColor">
           {headingLine}
         </h1>
         <!-- Adding max-width to the paragraph to control its horizontal expansion -->
         <p class="text-md flex justify-start text-start items-start font-normal leading-snug sm:leading-snug md:leading-medium lg:leading-medium text-white max-w-md ">
           {para}
         </p>
-        <Button size="lg" className="mt-10">Book A Table</Button>
+        <Button size="lg" className="mt-52">Book A Table</Button>
       </div>
     </div>
   </main>
