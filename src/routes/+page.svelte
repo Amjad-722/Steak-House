a56c8a27253f3b4ac9758ff757c567663bfd0c16
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-30 17:14:34 +0500

    updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,12 +13,16 @@ import Navabr from "../components/Navbar/Navabr.svelte";
 
 <main>
   <section
-  class="h-screen max-h-full bg-cover bg-no-repeat bg-center bg-blend-overlay "
-  style="background-image: url('https://t3.ftcdn.net/jpg/05/73/69/98/360_F_573699890_jzicI81KNwKhGY6fDXE3tcHD3ISmCd1y.jpg'); background-color: rgba(0, 0, 0, 0.7);"
+  class="h-screen max-h-full bg-cover bg-no-repeat bg-center bg-blend-overlay relative"
+  style="background-image: url('https://t3.ftcdn.net/jpg/05/73/69/98/360_F_573699890_jzicI81KNwKhGY6fDXE3tcHD3ISmCd1y.jpg'); background-color: #232533; opacity: 0.9;"
 >
+  <!-- <div class="absolute inset-0 bg-[#232533] bg-opacity-70"></div>  -->
+  
+  <!-- Content goes here -->
   <Navabr />
   <Home />
 </section>
+
 <section>
   <ReserverTable/>
 </section>
