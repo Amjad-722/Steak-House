77adab154de97ea193aa1a2ab5abafc4486f8406
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-30 17:35:26 +0500

    updated

--- src/components/BG.svelte
+++ src/components/BG.svelte
@@ -5,8 +5,8 @@
   <main class="flex items-center justify-center">
     <!-- Section with fixed background image and specific height -->
     <section
-      class="bg-fixed bg-center bg-cover bg-no-repeat w-full h-[600px]"
-      style="background-image: url('https://d4t7t8y8xqo0t.cloudfront.net/resized/750X436/eazytrendz%2F2669%2Ftrend20200113060319.jpg');">
+      class="bg-fixed bg-center bg-cover bg-no-repeat bg-blend-overlay w-full h-[600px]"
+      style="background-image: url('https://images.unsplash.com/photo-1514933651103-005eec06c04b?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D');background-color: #232533; opacity: 0.9;">
     </section>
   </main>
   
